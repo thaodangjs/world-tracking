85bf27cf4700dec7374c372e3b1fe2e99af03b27
Thao Dang <thaodang.js@gmail.com>
2023-12-11 09:05:12 +0700

    Added react-leaflet & leaflet lib & implemented map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,28 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const position = [40, 0];
+
+  return (
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={position}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
